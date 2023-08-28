43147e97b534a2c5532e15b036b0aea10df3c437
Clara <clara.garcia.contact@gmail.com>
2023-08-28 22:10:21 +0200

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react'
 import Pokedex from './components/pokedex'
 
-
 const App: FC = (): JSX.Element => {
   return <Pokedex />
 }
--- src/components/pokedex.tsx
+++ src/components/pokedex.tsx
@@ -17,13 +17,18 @@ interface PkmProps {
       stat: {
          name: string;
       };
-      species: {};
-   }>;
+   }>
+}
+
+interface PkmSpecies {
+   flavor_text_entries: Array<{ flavor_text: string }>;
+   genera : Array<{genus : string}>
 }
 
 const Pokedex: FC = (): JSX.Element => {
    const [Pokemon, setPokemon] = useState<PkmProps | null>(null);
-   const [PkmId, setPkmId] = useState<number>(1);
+   const [PokemonSpecies, setPokemonSpecies] = useState<PkmSpecies | null>(null);
+   const [PkmId, setPkmId] = useState<number>(6);
 
    const getPokemon = async (): Promise<void> => {
       const response = await fetch(
@@ -33,21 +38,63 @@ const Pokedex: FC = (): JSX.Element => {
       setPokemon(data);
    };
 
+   const getPokemonSpecies = async (): Promise<void> => {
+      const response = await fetch(
+         `https://pokeapi.co/api/v2/pokemon-species/${PkmId}/`
+      );
+      const data = await response.json();
+      setPokemonSpecies(data);
+   }
+
    useEffect(() => {
       getPokemon();
+      getPokemonSpecies();
    }, []);
+
+   const typeColor = (type: string | undefined) => {
+      switch (type) {
+         case "fire":
+            return "red";
+         case "water":
+            return "blue";
+         case "ice":
+            return "teal";
+         case "grass":
+            return "green";
+         case "bug":
+            return "green";
+         case "flying":
+         case "normal":
+         case "steel":
+            return "grey";
+         case "electric":
+            return "yellow";
+         case "dark":
+         case "poison":
+         case "ghost":
+         case "psychic":
+            return "purple";
+         case "rock":
+         case "fighting":
+         case "ground":
+            return "orange";
+         case "fairy":
+         case "dragon":
+            return "pink";
+      }
+   };
 
    return (
       <>
          <>
             <div className="flex justify-center mt-4">
-               <div className="w-[80rem] h-[55rem] bg-red-700 rounded-xl flex">
+               <div className="w-[80rem] h-[55rem] bg-red-800 rounded-xl flex">
                   <div className="w-1/2 h-full border-r-2">
-                     <div className="w-100% p-8 border-b-2 flex items-center gap-2">
+                     <div className="w-100% p-8 border-b-2 flex items-center">
                         <div className="mr-8 rounded-full bg-cyan-500 w-20 h-20 border-white border-4"></div>
-                        <div className="rounded-full bg-red-600 w-8 h-8 border-white border-2"></div>
-                        <div className="rounded-full bg-yellow-400 w-8 h-8 border-white border-2"></div>
-                        <div className="rounded-full bg-green-500 w-8 h-8 border-white border-2"></div>
+                        <div className="rounded-full bg-red-500 w-8 h-8"></div>
+                        <div className="rounded-full ml-5 bg-yellow-400 w-8 h-8 "></div>
+                        <div className="rounded-full w-[4.5rem] h-[4.5rem]"><img src="../public/green.gif" alt="" /></div>
                      </div>
                      <div className="flex justify-center items-center py-12">
                         <div className="w-fit bg-slate-100 rounded-lg">
@@ -70,13 +117,13 @@ const Pokedex: FC = (): JSX.Element => {
                         </div>
                      </div>
                      <div className="px-24 flex justify-between">
-                        <div className="w-fit">
-                           <div className="rounded-t-lg bg-red-800 flex gap-3 text-white p-2">
+                        <div className="w-60">
+                           <div className="rounded-t-lg bg-red-800 flex gap-3 text-white p-2 border-white border-2">
                               <div className="font-bold">N°{Pokemon?.id}</div>
                               <div className="capitalize">{Pokemon?.name}</div>
                            </div>
                            <div className="rounded-b-lg bg-white p-2">
-                              <p>Dragon pokemon</p>
+                              <p>{PokemonSpecies?.genera[7].genus}</p>
                            </div>
                            <div></div>
                         </div>
@@ -93,9 +140,7 @@ const Pokedex: FC = (): JSX.Element => {
 
                   <div className="w-1/2 h-full">
                      <div className="bg-black border-white border-2 text-white m-14 p-4 rounded-lg">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Ut vitae sagittis leo, id ornare risus. Aliquam pharetra
-                        molestie purus, aliquet tempor erat ultrices quis.
+                        {PokemonSpecies?.flavor_text_entries[0].flavor_text}
                      </div>
 
                      <div className="grid grid-cols-6 grid-rows-2 bg-sky-600 border-black border-2 p-4 m-14 rounded-lg">
@@ -129,10 +174,12 @@ const Pokedex: FC = (): JSX.Element => {
                      </div>
 
                      <div className="flex gap-4 m-14">
-                        <div className="border-white border-2 text-white p-4 rounded-lg w-1/2 uppercase">
+                        <div className="border-white border-2 text-white p-4 rounded-lg w-1/2 uppercase" style={{ backgroundColor: typeColor(Pokemon?.types[0].type.name) }}>
                            {Pokemon?.types[0].type.name}
                         </div>
-                        {/* {Pokemon?.types[0].type.name !== null ? <div className='border-white border-2 text-white p-4 rounded-lg w-1/2 uppercase'>{Pokemon?.types[1].type.name}</div> : 'ok'} */}
+                        <div className="border-white border-2 text-white p-4 rounded-lg w-1/2 uppercase" style={{ backgroundColor: typeColor(Pokemon?.types[1].type.name) }}>
+                           {Pokemon?.types[1].type.name !== null ? Pokemon?.types[1].type.name : null}
+                        </div>
                      </div>
                   </div>
                </div>
